61f11a983dd27622e743c5487fc6e6cec28f5833
Patricia Strutz <68596271+PStrutz@users.noreply.github.com>
2022-10-21 21:45:39 -0700

    package as module

--- RDS.py
+++ RDS.py
@@ -1,28 +1,30 @@
 import matplotlib.pyplot as plt
 import random
 
-x = []
-y = []
 
-for a in range(0,3000):
-   i = random.uniform(0,1000)
-   j = random.uniform(0,1000)
+def rds_assist():
+   x = []
+   y = []
 
-   x.append(i)
-   y.append(j)
-   
+   for a in range(0,3000):
+      i = random.uniform(0,1000)
+      j = random.uniform(0,1000)
 
-for item in range(len(x)):
-    plt.plot(x[item],y[item], c="cyan", marker=".")
-    plt.plot(x[item]-10,y[item], c="red", marker=".")
+      x.append(i)
+      y.append(j)
 
-plt.show()
 
-for item in range(len(x)):
-    plt.plot(x[item],y[item], c="cyan", marker=".")
-    plt.plot(x[item]-20,y[item], c="red", marker=".")
+   for item in range(len(x)):
+       plt.plot(x[item],y[item], c="cyan", marker=".")
+       plt.plot(x[item]-10,y[item], c="red", marker=".")
 
-plt.show()
+   plt.show()
+
+   for item in range(len(x)):
+       plt.plot(x[item],y[item], c="cyan", marker=".")
+       plt.plot(x[item]-20,y[item], c="red", marker=".")
+
+   plt.show()
 
 
 
