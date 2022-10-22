1680c2f85cac0428c1351d42c82291fb544db38d
Patricia Strutz <68596271+PStrutz@users.noreply.github.com>
2022-10-21 21:47:30 -0700

    Update RDS.py

--- RDS.py
+++ RDS.py
@@ -6,6 +6,7 @@ def rds_assist():
    x = []
    y = []
 
+   # generate 3000 random dots and save their coordinates
    for a in range(0,3000):
       i = random.uniform(0,1000)
       j = random.uniform(0,1000)
@@ -13,13 +14,14 @@ def rds_assist():
       x.append(i)
       y.append(j)
 
-
+   # plot the dots in cyan and red, with a slight offset
    for item in range(len(x)):
        plt.plot(x[item],y[item], c="cyan", marker=".")
        plt.plot(x[item]-10,y[item], c="red", marker=".")
 
    plt.show()
 
+   # plot the coordinates again, with a larger offset
    for item in range(len(x)):
        plt.plot(x[item],y[item], c="cyan", marker=".")
        plt.plot(x[item]-20,y[item], c="red", marker=".")
